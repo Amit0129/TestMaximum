3e5e62e49acc58af1b5f7a9d5203fb4043e8aaaa
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-12 16:33:21 +0530

    Adding:UC1_Fins The Maximun Number Between Three Number

--- TestMaximum/TestMaximum/Program.cs
+++ TestMaximum/TestMaximum/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Maximum Test Program");
+
+            Console.WriteLine(MaximumNumberCheck.MaximumIntegerNumber(30, 30, 30));
+            
         }
     }
 }
