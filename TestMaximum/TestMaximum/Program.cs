2aa23328b10f6faac79551ed4fc7a9cbdf88bab0
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-12 17:11:44 +0530

    Refactor:RC1_ All Three Method To One Generic One Method

--- TestMaximum/TestMaximum/Program.cs
+++ TestMaximum/TestMaximum/Program.cs
@@ -6,9 +6,13 @@
         {
             Console.WriteLine("Maximum Test Program");
 
-            Console.WriteLine(MaximumNumberCheck.MaximumIntegerNumber(30, 30, 30));
-            Console.WriteLine(MaximumNumberCheck.MaximumFloatsNumber(30.4, 30.8, 30.9));
-            Console.WriteLine(MaximumNumberCheck.MaximumString("Amit", "Kumar", "Nayakk"));
+            //Console.WriteLine(MaximumNumberCheck.MaximumIntegerNumber(30, 30, 30));
+            //Console.WriteLine(MaximumNumberCheck.MaximumFloatsNumber(30.4, 30.8, 30.9));
+            //Console.WriteLine(MaximumNumberCheck.MaximumString("Amit", "Kumar", "Nayakk"));
+            GenericMaximum genericMaximum = new GenericMaximum();
+            Console.WriteLine(genericMaximum.GetMaximun<int>(10, 20, 30));
+            Console.WriteLine(genericMaximum.GetMaximun<double>(10.5, 20.9, 30.7));
+            Console.WriteLine(genericMaximum.GetMaximun<string>("10", "20", "50"));
             
         }
     }
